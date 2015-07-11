3456ef6c769e5f4592e5ca8e4e22f946dea838d0
chdprimary <chdprimary@gmail.com>
2015-07-11 06:21:21 -0500

    Created a simple image slider.

---
+++ image-slider/slider.js
@@ -0,0 +1,29 @@
+var Slider = function() { this.initialize.apply(this,arguments); }
+Slider.prototype = {
+	initialize: function(slider) {
+		this.ul = slider.children[0];
+		this.li = this.ul.children;
+
+		this.ul.style.width = (this.li[0].clientWidth * this.li.length) + 'px';
+
+		this.currentIndex = 0;
+	},
+
+	goTo: function(index) {
+		if (index < 0 || index > this.li.length-1)
+			return;
+
+		this.ul.style.left = '-' + (100 * index) + '%';
+
+		this.currentIndex = index;
+	},
+
+	goToPrev: function() {
+		this.goTo(this.currentIndex - 1);
+	},
+
+	goToNext: function() {
+		this.goTo(this.currentIndex + 1);
+	}
+}
+
