79aa0aa082abc663a60d51db6398dad836250cd5
chdprimary <chdprimary@gmail.com>
2015-07-15 12:36:55 -0500

    Updated website, which includes core functionality and basic styling. Also made a minor bug fix to gmaps to transfer Google Maps API over
    HTTPS

--- new-site/app.js
+++ new-site/app.js
@@ -1,5 +1,27 @@
 var app = angular.module('flapperNews', []);
 
 app.controller('MainCtrl', ['$scope', function($scope){
-	$scope.test = 'Hello World!';
+	$scope.posts = [
+		{title: 'posta', upvotes: 5 , link: 'https://www.google.com'},
+		{title: 'postb', upvotes: 2 , link: 'https://www.google.com'},
+		{title: 'postc', upvotes: 15, link: 'https://www.google.com'},
+		{title: 'postd', upvotes: 16 },
+		{title: 'postf', upvotes: 9  }
+	];
+
+	$scope.addPost = function() {
+		if($scope.postTitle) {
+			$scope.posts.push({
+				title: $scope.postTitle, 
+				link: $scope.postLink, 
+				upvotes: 0
+			});
+			$scope.postTitle = '';
+			$scope.postLink = '';
+		}
+	};
+
+	$scope.upvotePost = function(post) {
+		post.upvotes += 1;
+	};
 }]);
