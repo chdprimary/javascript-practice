cd582204a86710af24a0a7359b39fd723df9a506
Christian Dean <cdean@thezebra.com>
2022-10-06 16:04:10 -0500

    Add modal project code

--- javascript-complete-course-udemy/06-Modal/starter/script.js
+++ javascript-complete-course-udemy/06-Modal/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const overlayEl = document.querySelector('.overlay');
+const modalEl = document.querySelector('.modal');
+const closeModalEl = document.querySelector('.close-modal');
+const showModalEls = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modalEl.classList.remove('hidden');
+  overlayEl.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modalEl.classList.add('hidden');
+  overlayEl.classList.add('hidden');
+};
+
+for (let i = 0; i < showModalEls.length; i++)
+  showModalEls[i].addEventListener('click', openModal);
+
+closeModalEl.addEventListener('click', closeModal);
+overlayEl.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modalEl.classList.contains('hidden')) {
+        closeModal();
+    }
+});
