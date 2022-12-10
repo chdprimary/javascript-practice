a872143fd810b7b9ac9420021f3370a455f3fd49
Christian Dean <cdean@thezebra.com>
2022-12-09 21:04:44 -0600

    Learning prototypes, challenge 1

---
+++ javascript-complete-course-udemy/14-OOP/starter/challenge1.js
@@ -0,0 +1,43 @@
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+// 1. 
+const Car = function(make, speed) {
+    this.make = make;
+    this.speed = speed;
+}
+
+// 2. 
+Car.prototype.accelerate = function() {
+    this.speed += 10;
+    console.log(this.speed);
+}
+
+// 3. 
+Car.prototype.brake = function() {
+    this.speed -= 5;
+    console.log(this.speed);
+}
+
+// 4.
+const honda = new Car('Honda', 0);
+const toyota = new Car('Toyota', 10);
+honda.accelerate();
+toyota.brake();
+
+const bmw = new Car("BMW", 120);
+const mercedes = new Car("Mercedes", 95);
+bmw.accelerate();
+mercedes.brake();
--- javascript-complete-course-udemy/14-OOP/starter/script.js
+++ javascript-complete-course-udemy/14-OOP/starter/script.js
@@ -1,1 +1,51 @@
 'use strict';
+
+// Constructor functions
+// what happens under the hood when you call `new Constructor(...)`
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically returns {}
+const Person = function(firstName, birthYear) {
+    // Instance properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    // Never create a method inside a constructor function
+    // Each copy would carry a reference to the function
+    // Which is not how prototypal inheritance should work since the prototype owns the methods
+    // this.calcAge = function() {
+    //     console.log(2037 - this.birthYear);
+    // };
+}
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
+
+// Instead of creating the method in the constructor
+// Create it on the constructor prototype property
+Person.prototype.calcAge = function() {
+    console.log(2037 - this.birthYear);
+};
+console.log(jonas.calcAge());
+
+// The instance.__proto__ is the same object as the constructor.prototype object
+console.log(jonas.__proto__);
+console.log(Person.prototype)
+console.log(jonas.__proto__ === Person.prototype);
+
+// Note that the constructor.prototype property is not the constructor's own prototype!
+console.log(Person.__proto__);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
