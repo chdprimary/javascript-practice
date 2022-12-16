92038e064131c59db5537d86107cefdc6d435ced
Christian Dean <cdean@thezebra.com>
2022-12-16 09:00:13 -0600

    Sending XHR

--- javascript-complete-course-udemy/16-Asynchronous/starter/script.js
+++ javascript-complete-course-udemy/16-Asynchronous/starter/script.js
@@ -4,3 +4,14 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+console.log(request);
+request.open('GET', 'https://restcountries.com/v2/name/portugal');
+// this is async
+request.send();
+
+request.addEventListener('load', function () {
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+});
