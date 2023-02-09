b9241aa1e0e229915e6fd5bee8944767475af975
Christian Dean <cdean@thezebra.com>
2023-02-08 19:32:27 -0600

    Publish to Netlify, replace broken lib

--- javascript-complete-course-udemy/18-forkify/starter/src/js/views/recipeView.js
+++ javascript-complete-course-udemy/18-forkify/starter/src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './view.js';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -129,7 +129,7 @@ class RecipeView extends View {
             <use href="${icons}#icon-check"></use>
           </svg>
           <div class="recipe__quantity">${
-            ing.quantity ? new Fraction(ing.quantity).toString() : ''
+            ing.quantity ? fracty(ing.quantity) : ''
           }</div>
           <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
